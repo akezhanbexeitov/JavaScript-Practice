2cd8e547aa900f5df94a67d3be9f4e24889c5e93
Akezhan Bexeitov <doolath01@gmail.com>
2022-06-24 12:25:42 +0800

    Create object and manipulate it in console

--- exercises/Practice/03_07/script.js
+++ exercises/Practice/03_07/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /**
  * Practice: Building objects
  *
@@ -7,3 +8,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const phone = {
+  model: "iPhone Pro 11",
+  platform: {
+    os: "iOS 15.5",
+    chipset: "Apple A13 Bionic (7 nm+)",
+    cpu: "Hexa-core (2x2.65 GHz Lightning + 4x1.8 GHz Thunder)",
+    gpu: "Apple GPU (4-core graphics)",
+  },
+  memory: "512 GB",
+  price: 1249,
+};
