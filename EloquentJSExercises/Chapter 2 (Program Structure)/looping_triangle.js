e1cfbbf7e06bba8b5bbbfd02ad6bd2441ab5fd95
Akezhan Bexeitov <doolath01@gmail.com>
2022-07-06 12:34:49 +0800

    Add shorter answers

--- EloquentJSExercises/Chapter 2 (Program Structure)/looping_triangle.js
+++ EloquentJSExercises/Chapter 2 (Program Structure)/looping_triangle.js
@@ -15,3 +15,10 @@ for (let i = 0; i < 7; i++) {
     console.log(hashtag)
 }
 console.log('')
+
+
+// Shorter answer
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+  
+console.log('')
