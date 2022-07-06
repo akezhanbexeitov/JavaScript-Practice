d8514260e61368788ea6258abae45c1deeebf687
Akezhan Bexeitov <doolath01@gmail.com>
2022-07-06 12:43:44 +0800

    Complete exercise 1

---
+++ EloquentJSExercises/Chapter 3 (Functions)/minimum.js
@@ -0,0 +1,21 @@
+function min (num1, num2) {
+    if (num1 < num2) {
+        return num1
+    } else if (num2 < num1) {
+        return num2
+    } else {
+        return num1 || num2
+    }
+}
+
+console.log(min(1, 1))
+console.log(min(4, 2))
+console.log(min(1, 5))
+console.log(min(0, 10))
+console.log(min(0, -10))
+
+// Shorter answer
+// function min(a, b) {
+//     if (a < b) return a;
+//     else return b;
+// }
