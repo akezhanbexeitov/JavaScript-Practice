d8d3907dd779c9a489d1027bc469d88c039d74b9
Akezhan Bexeitov <doolath01@gmail.com>
2022-07-12 16:13:20 +0800

    Change miltiple ul tags to li, create ul element

--- challenges/add_new_element.js
+++ challenges/add_new_element.js
@@ -1,11 +1,16 @@
+let navItems = `
+    <li class='nav-item'><a href='#'> Home</a></li>
+    <li class='nav-item'><a href='#'> About Us</a></li>
+    <li class='nav-item'><a href='#'> F.A.Q.</a></li>
+    <li class='nav-item'><a href='#'> Contact Us</a></li>
+    <li class='nav-item'><a href='#'> Address</a></li>
+`
+
 let navBar = document.createElement('nav')
 navBar.classList.add('nav-menu')
-let menuItems = `
-    <ul class='nav-item'><a href='#'> Home</a></ul>
-    <ul class='nav-item'><a href='#'> About Us</a></ul>
-    <ul class='nav-item'><a href='#'> F.A.Q.</a></ul>
-    <ul class='nav-item'><a href='#'> Contact Us</a></ul>
-    <ul class='nav-item'><a href='#'> Address</a></ul>
-`
-navBar.innerHTML = menuItems
+
+let navList = document.createElement('ul')
+navList.innerHTML = navItems
+
+navBar.append(navList)
 document.querySelector('header').append(navBar)
