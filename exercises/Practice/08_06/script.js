44239bd561d5a18e20634919fc5d92a15b7257f9
Akezhan Bexeitov <doolath01@gmail.com>
2022-07-13 15:30:33 +0800

    Complete practice 08_06

--- exercises/Practice/08_06/script.js
+++ exercises/Practice/08_06/script.js
@@ -5,3 +5,21 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// Create a basic function declaration
+function changeBackgroundColor(tag_name, color) {
+    document.querySelector(tag_name).style.backgroundColor = color
+}
+changeBackgroundColor('h1', 'black')
+
+// Create a basic function expression
+const changeColor = function (tag_name, color) {
+    document.querySelector(tag_name).style.color = color
+}
+changeColor('h2', 'white')
+
+// Create an arrow function
+const changeFontSize = (tag_name, size) => {
+    document.querySelector(tag_name).style.fontSize = `${size}px`
+}
+changeFontSize('p', 40)
